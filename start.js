39acbecf62b52181633defc644ed118b0f98e217
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-29 06:43:03 -0500

    update

--- start.js
+++ start.js
@@ -5,7 +5,9 @@ module.exports = {
       method: "shell.run",
       params: {
         venv: "../env",                // Edit this to customize the venv folder path
-        env: { },                   // Edit this to customize environment variables (see documentation)
+        env: { 
+          CUDA_HOME: "{{envs.CONDA_PREFIX}}"
+        },                   // Edit this to customize environment variables (see documentation)
         path: "app/inference",                // Edit this to customize the path to start the shell from
         message: [
           "python gradio_server.py --profile {{args.profile}} --compile",    // Edit with your custom commands
