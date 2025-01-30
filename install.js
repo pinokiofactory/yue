c94834e323965da3c214a9c099889573a6d1f183
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-29 21:00:06 -0500

    update

--- install.js
+++ install.js
@@ -25,6 +25,7 @@ module.exports = {
       params: {
         uri: "torch.js",
         params: {
+          venv_python: "3.11",
           venv: "env",                // Edit this to customize the venv folder path
           path: "app",                // Edit this to customize the path to start the shell from
           // xformers: true   // uncomment this line if your project requires xformers
