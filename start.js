4f4ad0948e52efe59f7872384003aca8a975b436
cocktailpeanut <cocktailpeanuts@proton.me>
2025-02-02 15:36:09 -0500

    update

--- start.js
+++ start.js
@@ -12,7 +12,7 @@ module.exports = async (kernel) => {
           venv: "../env",                // Edit this to customize the venv folder path
           path: "app/inference",                // Edit this to customize the path to start the shell from
           message: [
-            "python gradio_server.py {{args.mode ? '--'+args.mode : ''}} --profile {{args.profile}} {{args.compile ? '--compile' : ''}}",    // Edit with your custom commands
+            "python gradio_server.py {{args.mode ? '--'+args.mode : ''}} --profile {{args.profile}}",
           ],
           on: [{
             // The regular expression pattern to monitor.
